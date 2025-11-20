af6ab9bfdab4e020be75dc5ab644895eaaffe440
LECTAKONE Eric <Eric.Lectakone@bordeaux-inp.fr>
2025-11-20 10:12:30 +0100

    row reading caract

--- src/parseData.py
+++ src/parseData.py
@@ -6,9 +6,10 @@ def parseCaract(year):
     file = 'caract-'+year+'.csv'
     dir = os.path.dirname(__file__)
     path = os.path.normpath(os.path.join(dir,'..','data',year,file))
-    print(path)
-    with open(file=path):
-        pass
+    with open(file=path, newline='') as caracfile:
+        caracreader = csv.reader(caracfile, delimiter=';', quotechar='|')
+        for row in caracreader:
+            print(', '.join(row))
 
 
 if __name__ == "__main__":
