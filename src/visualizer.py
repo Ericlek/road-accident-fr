0c6480636d1f5c0f88e3f69b8ba5a37208cf057f
LECTAKONE Eric <Eric.Lectakone@bordeaux-inp.fr>
2025-11-26 17:20:40 +0100

    feat: Plot for severity by weather conditions

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 import parseData
 
@@ -22,7 +23,36 @@ def accidentByAtmos(caractData):
     plt.hist(atmo, bins = 10)
     plt.show()
 
+def severityByAtmos(caractData, usagersData):
+    accidents = {}
+    for acc_id, data in caractData.items():
+        accidents[acc_id] = [data[9],usagersData[acc_id][5]] # Weather condition / Accident's severity
+
+    plotData = {1: [0 for _ in range(9)],
+                2: [0 for _ in range(9)],
+                3: [0 for _ in range(9)],
+                4: [0 for _ in range(9)]}
+
+
+    for acc_id, data in accidents.items():
+        plotData[int(data[1])][int(data[0]) - 1] += 1
+    print(plotData)
+
+    df = pd.DataFrame([["Unharmed"] + plotData[1], ["Light wounds"] + plotData[4],
+                       ["Hospitalized"] + plotData[3], ["Dead"] + plotData[2]],
+                       columns=["Severity", "Normal", "Light rain", "Heavy rain", "Snow",
+                                "Fog", "Heavy storm", "Dazzling light", "Cloudy", "Others"])
+
+    df.plot(x='Severity', kind='bar', stacked=True,
+        title='Severity of accidents by weather conditions')
+    
+    plt.show()
+
+    # print(accidents)
+
 if __name__ == "__main__":
     caracDict = parseData.parseCaract(2024)
+    usagersDict = parseData.parseUsagers(2024)
+    severityByAtmos(caracDict, usagersDict)
     accidentByAtmos(caracDict)
     # accidentByHour(caracDict)
