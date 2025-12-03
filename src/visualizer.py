5c7ca46e3730d2a93745d654a6be441032845fac
LECTAKONE Eric <Eric.Lectakone@bordeaux-inp.fr>
2025-12-03 11:49:49 +0100

    changed severity by weather visualization

--- src/visualizer.py
+++ src/visualizer.py
@@ -36,23 +36,39 @@ def severityByAtmos(caractData, usagersData):
 
     for acc_id, data in accidents.items():
         plotData[int(data[1])][int(data[0]) - 1] += 1
-    print(plotData)
-
-    df = pd.DataFrame([["Unharmed"] + plotData[1], ["Light wounds"] + plotData[4],
-                       ["Hospitalized"] + plotData[3], ["Dead"] + plotData[2]],
-                       columns=["Severity", "Normal", "Light rain", "Heavy rain", "Snow",
-                                "Fog", "Heavy storm", "Dazzling light", "Cloudy", "Others"])
-
-    df.plot(x='Severity', kind='bar', stacked=True,
-        title='Severity of accidents by weather conditions')
     
+    df = pd.DataFrame({
+        "Severity": ["Unharmed", "Light wounds", "Hospitalized", "Dead"],
+        "Normal": [plotData[1][0], plotData[4][0], plotData[3][0], plotData[2][0]],
+        "Light rain": [plotData[1][1], plotData[4][1], plotData[3][1], plotData[2][1]],
+        "Heavy rain": [plotData[1][2], plotData[4][2], plotData[3][2], plotData[2][2]],
+        "Snow": [plotData[1][3], plotData[4][3], plotData[3][3], plotData[2][3]],
+        "Fog": [plotData[1][4], plotData[4][4], plotData[3][4], plotData[2][4]],
+        "Heavy storm": [plotData[1][5], plotData[4][5], plotData[3][5], plotData[2][5]],
+        "Dazzling light": [plotData[1][6], plotData[4][6], plotData[3][6], plotData[2][6]],
+        "Cloudy": [plotData[1][7], plotData[4][7], plotData[3][7], plotData[2][7]],
+        "Others": [plotData[1][8], plotData[4][8], plotData[3][8], plotData[2][8]],
+    })
+    
+    df_t = df.set_index("Severity").T
+
+    df_t.plot(kind='bar', stacked=True,
+        title='Severity of accidents by weather conditions')
     plt.show()
 
-    # print(accidents)
+    df_norm = df_t.div(df_t.sum(axis=1), axis=0) * 100  # Convert to %
+    df_norm.plot(kind="bar", stacked=True, figsize=(10,6),
+                 title="Severity of Accidents by Weather (Percentage)")
+    plt.xlabel("Weather")
+    plt.ylabel("Percentage of Accidents")
+    plt.xticks(rotation=45)
+    plt.legend(title="Severity")
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     caracDict = parseData.parseCaract(2024)
     usagersDict = parseData.parseUsagers(2024)
     severityByAtmos(caracDict, usagersDict)
-    accidentByAtmos(caracDict)
-    accidentByHour(caracDict)
+    # accidentByAtmos(caracDict)
+    # accidentByHour(caracDict)
